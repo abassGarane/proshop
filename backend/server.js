24c8c244e1ee5584371a7936e4bf0868e8a13b54
abassGarane <thelibertinewreck@gmail.com>
2021-05-01 13:39:20 +0300

    Spelling errors

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import products from './data/products.js';
 import dotenv from 'dotenv';
-import connectDB from '../congig/db.js';
+import connectDB from '../config/db.js';
 const app = express();
 dotenv.config();
 connectDB();
