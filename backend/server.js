05ade6cf9628cb416420d6ecf3f415630a23d40a
abassGarane <thelibertinewreck@gmail.com>
2021-05-02 12:21:22 +0300

    instantiating error handling middleware

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,9 @@ import products from './data/products.js';
 import dotenv from 'dotenv';
 import connectDB from '../config/db.js';
 import productRoutes from './routes/productRoutes.js';
+import { notFound, errorHandler } from './middleware/errorMIddleware.js';
 const app = express();
+
 dotenv.config();
 connectDB();
 
@@ -11,6 +13,9 @@ app.get('/', (req, res) => {
   res.send('API is running');
 });
 app.use('/api/products', productRoutes);
+
+app.use(notFound);
+app.use(errorHandler);
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on ${PORT} in ${process.env.NODE_ENV}`);
