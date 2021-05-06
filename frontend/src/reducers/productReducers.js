b3188cf4fcadec29fa1cb86dfcb6554553b948a7
abassGarane <thelibertinewreck@gmail.com>
2021-05-06 20:50:13 +0300

    adding user reducers

--- frontend/src/reducers/productReducers.js
+++ frontend/src/reducers/productReducers.js
@@ -5,7 +5,7 @@ import {
   PRODUCT_DETAILS_REQUEST,
   PRODUCT_DETAILS_SUCCESS,
   PRODUCT_DETAILS_FAIL,
-} from '../constants/constants';
+} from '../constants/productConstants';
 export const productListReducers = (state = { products: [] }, action) => {
   switch (action.type) {
     case PRODUCT_LIST_REQUEST:
