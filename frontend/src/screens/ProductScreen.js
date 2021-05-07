a78a6491cc401f92d0e41151c9b40f3ce11e7063
abassGarane <thelibertinewreck@gmail.com>
2021-05-07 22:00:47 +0300

    Adding the cart screen

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -15,7 +15,7 @@ import Loader from '../components/Loader';
 import Message from '../components/Message';
 import { listProductDetails } from '../actions/productActions';
 const ProductScreen = ({ match, history }) => {
-  const [qty, setQty] = useState(0);
+  const [qty, setQty] = useState(1);
   const dispatch = useDispatch();
   const productDetails = useSelector((state) => state.productDetails);
   const { loading, errors, product } = productDetails;
